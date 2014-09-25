fbff7800c3e3f90fb6fc648e0b3782eb2f4e45ee
lavkesh1608 <lavkesh.agrawal@techvalens.com>
2014-09-25 19:04:23 +0530

    updated on 25 sep 2014

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var ejs = require('ejs');
 
 
-var ejslocals = require('express-ejs-layouts');
+//var ejslayouts = require('express-ejs-layouts');
 var logger = require('morgan');
 var supervisor = require('supervisor');
 var cookieParser = require('cookie-parser');
@@ -17,12 +17,14 @@ var mod_user=require('./lib/user/index');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, '/public')));
+console.log('this public path'+__dirname);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('view options', { layout:'layout.ejs' });
+//app.set('view options', { layout:'layout.ejs' });
 //app.use(ejs);
-app.use(ejslocals);
+//app.use(ejslayouts);
 app.use(mod_user);
 
 // uncomment after placing your favicon in /public
@@ -31,7 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
